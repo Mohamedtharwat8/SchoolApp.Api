07b791467890d22980eec1701763814aacb9e981
Mohamed Tharwat <Mohamedtharwat8>
2025-08-22 23:06:45 +0300

    Add user registration endpoint and update configurations
    - Implemented a new `Register` method in `AuthenticationController.cs` for user registration, including validation and user creation logic.
    - Updated JWT secret key and issuer URL in `appsettings.json`.
    - Created a new `RegisterVM.cs` file for user registration data transfer.
    - Added a migration class `FixIdentityIssue` as a placeholder for future schema changes.
    - Generated `20250822195825_FixIdentityIssue.Designer.cs` to define the database model for identity tables.

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using SchoolApp.Api.Data;
+using SchoolApp.Api.Data.ViewModels;
 using SchoolApp.Api.Models;
+using System.Threading.Tasks;
 
 namespace SchoolApp.Api.Controllers
 {
@@ -28,7 +30,36 @@ namespace SchoolApp.Api.Controllers
             _context = context;
             _configuration = configuration;
         }
+        [HttpPost("register-user")]
+        public async Task<IActionResult> Register([FromBody] RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Please, provide all the required fields");
 
+            }
+            var userExists = await _userManager.FindByNameAsync(registerVM.UserName);
+            if(userExists != null)
+            {
+                return BadRequest($"User {registerVM.EmailAddress} Already Exsits");
+            }
+            ApplicationUser newUser = new ApplicationUser()
+            {
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.UserName,
+                SecurityStamp = System.Guid.NewGuid().ToString()
+
+            };
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if(!result.Succeeded)
+            {
+                return BadRequest("User creation failed! Please check user details and try again.");
+            }
+            return Ok($"User {registerVM.EmailAddress} Created Successfully!");
+
+        }
 
     }
 }
---
+++ Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.Api.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(6)] 
+        public string Password { get; set; } 
+    }
+}
